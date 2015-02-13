1b32b5209102b453074ee641452f79ec4eb6f424
Dan Davies <shout@dan-davies.co.uk>
2015-02-13 09:45:41 +0000

    Added GULP to the project

---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+// include gulp
+var gulp = require('gulp'); 
+var changed = require('gulp-changed');
+var imagemin = require('gulp-imagemin');
+var concat = require('gulp-concat');
+var stripDebug = require('gulp-strip-debug');
+var uglify = require('gulp-uglify');
+var autoprefix = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
+ 
+// include plug-ins
+var jshint = require('gulp-jshint');
+ 
+// JS hint task
+gulp.task('jshint', function() {
+  gulp.src('./src/scripts/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// minify new images
+gulp.task('imagemin', function() {
+  var imgSrc = './assets/img/**/*',
+      imgDst = './build/assets/img/';
+ 
+  gulp.src(imgSrc)
+    .pipe(changed(imgDst))
+    .pipe(imagemin())
+    .pipe(gulp.dest(imgDst));
+});
+
+// include plug-ins
+var minifyHTML = require('gulp-minify-html');
+ 
+// minify new or changed HTML pages
+gulp.task('htmlpage', function() {
+  var htmlSrc = './*.html',
+      htmlDst = './build';
+ 
+  gulp.src(htmlSrc)
+    .pipe(changed(htmlDst))
+    .pipe(minifyHTML())
+    .pipe(gulp.dest(htmlDst));
+});
+
+// JS concat, strip debugging and minify
+gulp.task('scripts', function() {
+  gulp.src(['./assets/js/lib.js','./assets/js/*.js'])
+    
+    .pipe(stripDebug())
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/assets/js/'));
+});
+
+// CSS concat, auto-prefix and minify
+gulp.task('styles', function() {
+  gulp.src(['./assets/css/*.css'])
+    .pipe(concat('base.css'))
+    .pipe(autoprefix('last 2 versions'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./build/assets/css/'));
+});
+
+// default gulp task
+gulp.task('default', ['imagemin', 'htmlpage', 'scripts', 'styles'], function() {
+});
+
+// default gulp task
+gulp.task('default', ['imagemin', 'htmlpage', 'scripts', 'styles'], function() {
+  // watch for HTML changes
+  gulp.watch('./*.html', function() {
+    gulp.run('htmlpage');
+  });
+ 
+  // watch for JS changes
+  gulp.watch('./assets/js/*.js', function() {
+    gulp.run('jshint', 'scripts');
+  });
+ 
+  // watch for CSS changes
+  gulp.watch('./assets/css/*.css', function() {
+    gulp.run('styles');
+  });
+});
